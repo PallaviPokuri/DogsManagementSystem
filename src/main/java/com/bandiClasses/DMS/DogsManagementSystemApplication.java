563ee632cae2ca32df0a51fa8b8dc0fcca6f300e
PallaviPokuri <pokuripallavi2002@gmail.com>
2024-11-20 22:43:59 -0600

    update

---
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.bandiClasses.DMS;
+/**
+ * @author S565489 Pallavi Pokuri
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
